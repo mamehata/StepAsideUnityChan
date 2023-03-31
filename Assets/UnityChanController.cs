fe85375f565aa175f19acffd74bd68a33aa68ca7
mamehata <117148042+mamehata@users.noreply.github.com>
2023-03-31 23:21:09 +0900

    ユニティちゃんが車・カラーコーンに衝突した時とゴールに到達した時のスクリプトを作成
    テックアカデミーカリキュラムLesson 6Chapter 6.3

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -16,6 +16,10 @@ public class UnityChanController : MonoBehaviour
     private float velocityY = 10f;
     //左右の移動できる範囲（追加）
     private float movableRange = 3.4f;
+    //動きを減速させる係数（追加）
+    private float coefficient = 0.99f;
+    //ゲーム終了の判定（追加）
+    private bool isEnd = false;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +38,16 @@ public class UnityChanController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //ゲーム終了ならUnityちゃんの動きを減衰する（追加）
+        if (this.isEnd)
+        {
+            this.velocityZ *= this.coefficient;
+            this.velocityX *= this.coefficient;
+            this.velocityY *= this.coefficient;
+            this.myAnimator.speed *= this.coefficient;
+        }
+
         //横方向の入力による速度（追加）
         float inputVelocityX = 0;
         //上方向の入力による速度（追加）
@@ -73,5 +87,21 @@ public class UnityChanController : MonoBehaviour
 
         //Unityちゃんに速度を与える（変更）
         this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, this.velocityZ);
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理（追加）
+    void OnTriggerEnter(Collider other)
+    {
+
+        //障害物に衝突した場合（追加）
+        if (other.gameObject.CompareTag("CarTag") || other.gameObject.CompareTag("TrafficConeTag"))
+        {
+            this.isEnd = true;
+        }
+        //ゴール地点に到達した場合（追加）
+        else if (other.gameObject.CompareTag("GoalTag"))
+        {
+            this.isEnd = true;
+        }
     }
 }
